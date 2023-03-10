2448079b5edbdb43ce5cf37bb0486eb0b3fa0e3a
manoeltitto <manoeltito@hotmail.com>
2023-03-10 13:27:20 -0300

    First Commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,7 +8,43 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         Gato gato2 = new Gato();
 
+        Livro livro = new Livro("O Problema dos três copos: ", 200);
+        System.out.println(livro);
         System.out.println(gato);
         System.out.println(gato2);
+    }
+}
+
+class Livro{
+    private String Nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        Nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "Nome='" + Nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
