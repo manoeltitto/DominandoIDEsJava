1e8f1271d975ebb0c65fe376ed50c2b083923eb0
manoeltitto <manoeltito@hotmail.com>
2023-03-10 13:17:56 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        Gato gato2 = new Gato();
+
+        System.out.println(gato);
+        System.out.println(gato2);
+    }
+}
